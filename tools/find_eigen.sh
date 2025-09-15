98b29ce39f857b36460f23c3c5b545adc4203ebe
Bernd Bischl <bernd.bischl@gmx.net>
2025-09-15 12:33:27 +0200

    find_eigen

--- tools/find_eigen.sh
+++ tools/find_eigen.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 set -e
 
-mkdir -p src
-
 EIGEN_CFLAGS=""
 
+# check with "type" if command pkg-config is available
 if type pkg-config; then
   EIGEN_CFLAGS="$(pkg-config --cflags eigen3 2>/dev/null)"
+  # if exit status is not 0 AND cmake is available, try to find eigen with cmake
   if [ $? -ne 0 ] && type cmake; then
+    # COMPILER_ID seems to be necessary, but this should not really influence things
+    # in a non-portable way at this stage as we only ask cmake for the include flags
     EIGEN_CFLAGS="$(cmake --find-package -DCOMPILER_ID=GNU -DNAME=Eigen3 -DLANGUAGE=CXX -DMODE=COMPILE 2>/dev/null)"
+    # if we fail, set EIGEN_CFLAGS to empty
     if [ $? -ne 0 ]; then
       EIGEN_CFLAGS=
     fi
